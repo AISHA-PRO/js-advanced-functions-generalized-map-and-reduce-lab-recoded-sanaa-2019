c7ca7365bf93a87eef9efdf1bf81a5cb731c287d
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-08 17:02:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Add your functions here
-const newArray = function map(Array, fun){
-  return Array.map(fun);
+const newArray = function map(array, fun){
+  return array.map(fun);
+}
+const newArray2 = function reduce(array,fun,startPoint){
+  return array.reduce(array)
 }
